697d4fd3b88da6d81098210f58d11695f147186a
XXXiV-QN <53305147+XXXiV-QN@users.noreply.github.com>
2019-11-14 09:30:20 -0500

    update
    still having trouble to install scipy

--- scipy installe .py
+++ scipy installe .py
@@ -1,1 +1,1 @@
--m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+sudo port install py35-numpy py35-scipy py35-matplotlib py35-ipython +notebook py35-pandas py35-sympy py35-nose
