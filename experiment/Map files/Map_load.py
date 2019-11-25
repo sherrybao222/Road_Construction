57daf6d4540ae0cc0a923a51dffc5647cee45623
XXXiV-QN <53305147+XXXiV-QN@users.noreply.github.com>
2019-11-25 14:47:06 -0500

    Update Map_load.py

--- experiment/Map files/Map_load.py
+++ experiment/Map files/Map_load.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-data = pd.read_csv("trial.csv") # access the 4th_col of city_xy from file = pd.read_csv("trial.csv", usecols=[3])
-# data = data.iloc[0]
-trial = 10
-#
-for trial in range(0, trial):
-    # trial_i = trial.__index__()
-    if input("next trial") == str(1):
-        data_row = data.loc[trial]
-        data_col = data_row[['City_xy']]
-        print(data_col)
+# data = pd.read_csv("trial.csv") # access the 4th_col of city_xy from file = pd.read_csv("trial.csv", usecols=[3])
+# # data = data.iloc[0]
+# trial = 10
+# #
+# for trial in range(0, trial):
+#     # trial_i = trial.__index__()
+#     if input("next trial") == str(1):
+#         data_row = data.loc[trial]
+#         data_col = data_row[['City_xy']]
+#         print(data_col)
+
+
