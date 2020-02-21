701088ae0ee62e9c656c706443a2305cfca6dcdb
XXXiV-QN <53305147+XXXiV-QN@users.noreply.github.com>
2020-02-21 10:06:24 -0500

    update instruction

--- experiment/training.py
+++ experiment/training.py
@@ -12,33 +12,27 @@ def text_write(text, size, color, x, y,screen):  # function that can display any
 # instruction
 # =============================================================================
 def ins_1(screen): 
-    text_write('Welcome! ',50, BLACK, 50, 300, screen)
-    text_write('You are invited to be part of a research study to learn about spatial planning.', 50, BLACK, 50, 400,screen)
-    text_write('This study is conducted by Dr. Wei Ji Ma, in the Department of Psychology ', 50, BLACK, 50, 500,screen)
-    text_write('and the Center for Neural Science in the Faculty of Arts & Sciences', 50, BLACK, 50, 600,screen)
-    text_write('at New York University.', 50, BLACK, 50, 700, screen)
+    text_write('This study takes approximately 40-50 minutes to complete.',50, BLACK, 50, 300, screen)
+    text_write('It consists of 6 parts, and each part takes about 6-8 minutes.', 50, BLACK, 50, 400,screen)
+    text_write('You can take a short break after each part.', 50, BLACK, 50, 500,screen)
+    text_write('The researcher will now guide you through some instructions. ', 50, BLACK, 50, 600,screen)
     text_write('Press SPACE to continue', 50, BLACK, 50, 900, screen)
 
 def ins_2(screen): 
-    text_write('You need to complete 3 types of tasks in this study.',50, BLACK, 50, 300, screen)
-    text_write('The researcher will guide you through some instructions shortly.', 50, BLACK, 50, 400,screen)
-    text_write('This study takes approximately 40-50 minutes to complete.', 50, BLACK, 50, 500,screen)
+    text_write('In each part you will be doing one of the following 3 tasks:',50, BLACK, 50, 300, screen)
+    text_write('Number Estimation, Road Construction, and Road Construction with Undo.', 50, BLACK, 50, 400,screen)
+    text_write('Now we are going to show you the instructions for these 3 tasks. ', 50, BLACK, 50, 500,screen)
     text_write('Press SPACE to continue', 50, BLACK, 50, 900, screen)
 
 def incentive_1(screen):
-    text_write('You will be compensated $12 for your participation. ',50, BLACK, 50, 300, screen)
-    text_write('There will be up to $10 bonus added based on your performance in Road Construction.', 50, BLACK, 50, 400,screen)
-    text_write('We will randomly select 10 scores from your Road Construction performance.', 50, BLACK, 50, 500,screen)
-    text_write('Their sum will be multiplied by 0.01 as your bonus. ', 50, BLACK, 50, 600, screen)
-    text_write('Press SPACE to see an example', 50, BLACK, 50, 900, screen)
+    text_write('You will receive $12 for your participation. ',50, BLACK, 50, 300, screen)
+    text_write('There will be up to $10 bonus added based on your performance.', 50, BLACK, 50, 400,screen)
+    text_write('We will randomly select 3 scores from your Road Construction with/without Undo scores as the bonus.', 50, BLACK, 50, 500,screen)
+    text_write('Press SPACE to continue', 50, BLACK, 50, 900, screen)
 
-def incentive_2(screen):
-    text_write('For example, your 10 randomly selected scores are 64, 25, 49, 64, 36, 49, 49, 64, 100, 25',50, BLACK, 50, 300, screen)
-    text_write('Your total bonus score will be 525 ', 50, BLACK, 50, 400,screen)
-    text_write('You will be awarded with $5.25 in addition to your $12', 50, BLACK, 50, 500,screen)
-    text_write('Thus, you will receive a total of $17.25 upon completion ', 50, BLACK, 50, 600, screen)
-    text_write('Now, you are ready to start, and the researcher will leave the room.', 50, BLACK, 50, 700, screen)
-    text_write('Press SPACE to start. ', 50, BLACK, 50, 900, screen)
+def ins_end(screen):
+    text_write('Do you have any questions? ',50, BLACK, 50, 300, screen)
+    text_write('If not, press SPACE to start the experiment.', 50, BLACK, 50, 900, screen) 
 
 def training(screen):    
     # instruction
