4cc6ca66428f1affc770414fac6e34411b88df3d
XXXiV-QN <53305147+XXXiV-QN@users.noreply.github.com>
2020-02-28 15:03:44 -0500

    plot code

--- experiment/data_003/Qixiu_plot/json_spyder.py
+++ experiment/data_003/Qixiu_plot/json_spyder.py
@@ -5,34 +5,97 @@ Spyder Editor
 This is a temporary script file.
 """
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 
+#loading pilot data
+with open('/Users/fqx/Dropbox/Spring 2020/Ma Lab/GitHub/Road_Construction/experiment/data_002/test_all') as f:
+  d1 = json.load(f)
 with open('/Users/fqx/Dropbox/Spring 2020/Ma Lab/GitHub/Road_Construction/experiment/data_003/test_all_3') as f:
-  data = json.load(f)
+  d2 = json.load(f)
+# with open('/Users/fqx/Dropbox/Spring 2020/Ma Lab/GitHub/Road_Construction/experiment/data_004/test_all_4') as f:
+#   d3 = json.load(f)
 
-for i in range(10):
-    mapid = data[i]['mapid']
-    mapid = int(sum(mapid)/len(mapid))
-    #print(mapid)
+# list for operations
+d1_rc = []
+d2_rc = []
+d3_rc = []
 
-total_city = 0
-for i in range(20):
-    n_city = data[i]['n_city'][-1]
-    total_city += n_city
+d1_undo = []
+d2_undo = []
+d3_undo = []
 
-mean = total_city/20
-#print(mean)
+# d1_rc.clear()
+# d2_rc.clear()
+# d3_rc.clear()
+#
+# d1_undo.clear()
+# d2_undo.clear()
+# d3_undo.clear()
 
-con1 = 0
-con2 = 0
+# score list for RC & undo condition
 for i in range(144):
-    if data[i]['cond'][-1]==2:
-        n_city = data[i]['n_city'][-1]
-        con1 += n_city
-    if data[i]['cond'][-1]==3:
-        n_city = data[i]['n_city'][-1]
-        con2 += n_city
+    if d1[i]['cond'][-1] == 2:
+        d1_rc.append(d1[i]['n_city'][-1])
+    if d2[i]['cond'][-1] == 2:
+        d2_rc.append(d2[i]['n_city'][-1])
+    if d1[i]['cond'][-1] == 3:
+        d1_undo.append(d1[i]['n_city'][-1])
+    if d2[i]['cond'][-1] == 3:
+        d2_undo.append(d2[i]['n_city'][-1])
 
-mean1 = con1/144
-mean2 = con2/144
-print(mean1)
-print(mean2)
+rc_con = [d1_rc[i] + d2_rc[i] for i in range(len(d1_rc))]
+undo_con = [d1_undo[i] + d2_undo[i] for i in range(len(d1_undo))]
+
+print(rc_con)
+print(undo_con)
+
+rc_ave = [rc_con[i]/2 for i in range(len(rc_con))]
+undo_ave = [undo_con[i]/2 for i in range(len(undo_con))]
+
+print(rc_ave)
+print(undo_ave)
+
+# mapid list
+mapid = []
+for i in range(24):
+    mapid.append(d1[i]['mapid'][-1])
+print(mapid)
+
+plot1 = plt.plot(mapid, rc_ave[:24], label='RC')
+plot2 = plt.plot(mapid, undo_ave[:24],label='Undo')
+plt.legend(loc="upper left")
+plt.xlabel('map number')
+plt.ylabel('cities connected')
+plt.show()
+
+# mapid.hist(rc_ave, undo_ave, 24, density=True, histtype='bar', stacked=True)
+# mapid.set_title('stacked bar')
+# plt.show()
+
+
+# practice
+# for i in range(10):
+#     mapid = d1[i]['mapid']
+#     mapid = int(sum(mapid)/len(mapid))
+#     #print(mapid)
+#
+# total_city = 0
+# for i in range(20):
+#     n_city = d1[i]['n_city'][-1]
+#     total_city += n_city
+# mean = total_city/20
+#
+# con1 = 0
+# con2 = 0
+# for i in range(144):
+#     if d1[i]['cond'][-1]==2:
+#         n_city = d1[i]['n_city'][-1]
+#         con1 += n_city
+#     if d1[i]['cond'][-1]==3:
+#         n_city = d1[i]['n_city'][-1]
+#         con2 += n_city
+#
+# mean1 = con1/144
+# mean2 = con2/144
+print(d1[0].keys())
