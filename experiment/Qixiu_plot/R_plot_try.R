dff8d9d5eeffd46ae697c0f317ebc0a90a683936
XXXiV-QN <53305147+XXXiV-QN@users.noreply.github.com>
2020-02-28 16:13:55 -0500

    nvm

--- experiment/Qixiu_plot/R_plot_try.R
+++ experiment/Qixiu_plot/R_plot_try.R
@@ -4,3 +4,4 @@ con1 = mean(subset(test_exp_3, cond==2 & mapid)$n_city)
 sd(subset(test_exp_3, cond==2)$n_city)
 con2 = mean(subset(test_exp_3, cond==3 & mapid)$n_city)  
 sd(subset(test_exp_3, cond==3)$n_city)
+
