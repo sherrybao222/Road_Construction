c57843f2aa0b2825984dec5604bdbacfce0ee854
Dongqi Bao <sherrymoriarty@hotmail.com>
2020-04-10 00:07:23 -0400

    organize dataframe, add csv

--- plot/plot_data.py
+++ plot/plot_data.py
@@ -6,10 +6,12 @@ import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 import math
 import matplotlib.lines as mlines
-from numpy import matrix
+from statannot import add_stat_annotation
+import pandas as pd 
 
-data_all = []
-subs = [1,2,4]
+data_all = [] # prepare for all data
+subs = [1,2,4] # subject index
+
 # import experiment data
 for num in subs:
     with open('/Users/sherrybao/Downloads/Research/Road_Construction/rc_all_data/data_copy/data_pilot/sub_'+str(num)+'/test_all_'+str(num),'r') as file: 
@@ -26,57 +28,71 @@ with open('/Users/sherrybao/Downloads/Research/Road_Construction/rc_all_data/map
 num_ind = []
 rc_ind = []
 undo_ind = []
+
 # participant answer
 num = []
 rc = []
 undo = []
-n_undo = [] #number of undo
+
 # correct answer
-num_list = num_map[2]*3
+num_list = num_map[2]*3 # duplicate for all subjects
 rc_list = rc_map[2]*3
-budget_list = []
+
+undo_click = [] # if commit undo in a trial
+n_undo = [] #number of undo
+
+budget_list = [] #budget for number est
+
 # time for a trial
-t_rc = []
-t_undo = []
-f_t_rc = []
-f_t_undo = []
-undo_click = []
+t_rc = [] # whole trial
+t_undo = [] # whole trial
+f_t_rc = [] # first move
+f_t_undo = [] # first move
 
-rc_t_all = [] #choice index
-rcu_t_all = [] # choice and undo index
-u_t_all = [] # undo index
-rc_u_t_all = []
+# get time index for further calculatation of time of a single move
+rc_t_all = [] #choice index in basic trl
+rcu_t_all = [] # choice and undo index in undo trl
+u_t_all = [] # undo index in undo trl
+rc_u_t_all = [] # choice index in undo trl
 
+# time of a single move
 t_everyact_rc = [] # time for choice (exclude first choice) in basic
-t_everyact_undo = [] # time for every action (except submit) in basic
-t_everyundo = []
-t_everyc_undo = []
+t_everyact_undo = [] # time for every action (except first choice and submit) in undo
+t_everyundo = [] # time for undo
+t_everyc_undo = [] # time for every choice (exclude first choice) in undo
 
-t_s_rc = [] # time to submit
-t_s_undo = []
-# statistics list
-mean_rc = []
-mean_undo = []
+t_s_rc = [] # time to submit in basic
+t_s_undo = [] # time to submit in undo
+
+
+# across subject statistics summary
+mean_rc = [] # mean of number of connect city in rc
+mean_undo = [] # mean of number of connect city in undo trl
 ac_num = [] # accuracy for num_est
+budget_numest = [] # accuracy for num_est for every budget
 pc_undo = [] # Percentage of using undos among all undo trials
-mean_t_rc = []
-mean_t_undo = []# Average time spent in RC VS RCU across maps
-mean_f_t_rc = []
-mean_f_t_undo = []# Average time spent in RC VS RCU across maps
+mean_t_rc = [] # mean of whole trial time for rc trl
+mean_t_undo = [] # mean of whole trial time for undo trl
+mean_f_t_rc = [] # mean of first move time for rc trl
+mean_f_t_undo = []# mean of first move time for undo trl
 
 
 
 for data in data_all:
-    for i in range(len(data[0])):
+    for i in range(len(data[0])): # index for every trial
         if data[0][i]['cond'][-1] == 1:
             num.append(int(data[0][i]['num_est'][-1]))
             budget_list.append(data[0][i]['total'])
+            
             num_ind.append(i)
+            
         if data[0][i]['cond'][-1] == 2:
             rc.append(data[0][i]['n_city'][-1])
             t_rc.append(data[0][i]['time'][-1]-data[0][i]['time'][0])
+            
+           
             ind_choice = next(x for x, val in enumerate(data[0][i]['choice_his']) 
-                                  if val != 0)
+            if val != 0)  # index for first move
             f_t_rc.append(data[0][i]['time'][ind_choice]-data[0][i]['time'][0])
             
             v = np.array(data[0][i]['choice_his'])
@@ -86,13 +102,16 @@ for data in data_all:
             temp = [data[0][i]['time'][rc_t_all[-1][p+1]]-data[0][i]['time'][rc_t_all[-1][p]] for p,x in enumerate(rc_t_all[-1]) if p<len(rc_t_all[-1])-1]
             t_everyact_rc.append(temp)
             
-            t_s_rc.append(data[0][i]['time'][-1]-data[0][i]['time'][rc_t_all[-1][-1]])
+            t_s_rc.append(data[0][i]['time'][-1]-data[0][i]['time'][rc_t_all[-1][-1]])           
+            
             rc_ind.append(i)
+            
         if data[0][i]['cond'][-1] == 3:
             undo.append(data[0][i]['n_city'][-1])
             t_undo.append(data[0][i]['time'][-1]-data[0][i]['time'][0])
+            
             ind_choice = next(x for x, val in enumerate(data[0][i]['choice_his']) 
-                                  if val != 0)
+                                  if val != 0)# index for first move
             f_t_undo.append(data[0][i]['time'][ind_choice]-data[0][i]['time'][0])
             
             v = np.array(data[0][i]['choice_his'])
@@ -109,32 +128,38 @@ for data in data_all:
             u_t_all.append([i for i,x in enumerate(data[0][i]['undo_press']) 
                                         if x == 1])
             
-            np_rcu_t_all = np.array(rcu_t_all[-1])
+            np_rcu_t_all = np.array(rcu_t_all[-1])# change to numpy array for next opertation
             t_everyundo.append([t_everyact_undo[-1][np.where(np_rcu_t_all == x)[0][0]-1]for x in u_t_all[-1]])
             rc_u_t_all.append([e for e in rcu_t_all[-1] if e not in u_t_all[-1]])
             t_everyc_undo.append([e for e in t_everyact_undo[-1] if e not in t_everyundo[-1]])
 
-            n_undo.append(sum(data[0][i]['undo_press']))
             t_s_undo.append(data[0][i]['time'][-1]-data[0][i]['time'][rcu_t_all[-1][-1]])
+
+            n_undo.append(sum(data[0][i]['undo_press']))
+            
             if 1 in set(data[0][i]['undo_press']):
                 undo_click.append(1)
             else:
                 undo_click.append(0)
+                
             undo_ind.append(i)
 
-bool_numest = map(eq, num, num_list) 
+bool_numest = map(eq, num, num_list) # correct or not in num_est 
 int_numest = list(np.array(list(bool_numest)).astype(float))# indicator num_est is correct
 
-mx_num = [0]*3
-sorted_mx = [0]*3
+# initialize
+mx_num = [0]*3 # number est summary matrix 
+sorted_mx = [0]*3 # number est summary matrix (sorted by budget)
 
 for i in range(0,3):
     mean_rc.append(mean(rc[48*i:48*(i+1)]))
     mean_undo.append(mean(undo[48*i:48*(i+1)]))
+    
     mean_t_rc.append(mean(t_rc[48*i:48*(i+1)]))
     mean_t_undo.append(mean(t_undo[48*i:48*(i+1)]))
     mean_f_t_rc.append(mean(f_t_rc[48*i:48*(i+1)]))
     mean_f_t_undo.append(mean(f_t_undo[48*i:48*(i+1)]))
+    
     pc_undo.append(mean(undo_click[48*i:48*(i+1)]))
     ac_num.append(mean(int_numest[48*i:48*(i+1)]))
 
@@ -142,50 +167,59 @@ for i in range(0,3):
     temp = mx_num[i].view(np.ndarray)
     sorted_mx[i] = temp[np.lexsort((temp[:, 1], ))] # sort by budget
 
-budget_numest = []
 for i in range(0,3):
     sub = []
     for j in range(0,3):
         sub.append(mean(sorted_mx[i][16*j:16*(j+1),4]))
     budget_numest.append(sub)
-    
+
+# calculated for heatmap, ignore for now    
 rc_undo = np.zeros((12,12))
 for ind,val in enumerate(rc):
         rc_undo[val,undo[ind]] = rc_undo[val,undo[ind]]+1/len(rc)
-num_mx = np.zeros((11,11))
-for ind,val in enumerate(num):
-        num_mx[val,num_list[ind]] = num_mx[val,num_list[ind]]+1/len(num)
-rc_mx = np.zeros((12,12))
-for ind,val in enumerate(rc):
-        rc_mx[val,rc_list[ind]] = rc_mx[val,rc_list[ind]]+1/len(rc)
 
-diff = []
+# difference
+diff = [] # number connected (actual - max) in rc
 zip_obj = zip(rc,rc_list)
 for x,y in zip_obj:
     diff.append(x-y)
     
-diff_undo = []
+diff_undo = [] # number connected (actual - max) in undo
 zip_obj = zip(undo,rc_list)
 for x,y in zip_obj:
     diff_undo.append(x-y)
     
-diff_n = []
+diff_n = [] # number estimation (reported - correct) 
 zip_obj = zip(num,num_list)
 for x,y in zip_obj:
     diff_n.append(x-y)
     
-rm_t = []
-zip_obj = zip(t_rc,f_t_rc)
-for x,y in zip_obj:
-    rm_t.append(x-y)
+#rm_t = []
+#zip_obj = zip(t_rc,f_t_rc)
+#for x,y in zip_obj:
+#    rm_t.append(x-y)
+#
+#rm_t_undo = []
+#zip_obj = zip(t_undo,f_t_undo)
+#for x,y in zip_obj:
+#    rm_t_undo.append(x-y)
 
-rm_t_undo = []
-zip_obj = zip(t_undo,f_t_undo)
-for x,y in zip_obj:
-    rm_t_undo.append(x-y)
+# organize some data into dataframe  
+# intialise data of lists.
 
+sub = [x for s in subs for x in [s]*48] 
+data = {'sub':sub, 'num_ind':num_ind, 'rc_ind':rc_ind, 'undo_ind':undo_ind,
+        'num_list':num_list,  'budget_list':budget_list, 'rc_list':rc_list,
+        'undo_click':undo_click, 'n_undo':n_undo, 'diff_n':diff_n, 'diff':diff,
+        'diff_undo':diff_undo, 't_rc':t_rc, 't_undo':t_undo, 'f_t_rc':f_t_rc,
+        't_everyact_rc':t_everyact_rc,'t_everyundo':t_everyundo, 't_everyc_undo':t_everyc_undo,
+        't_s_rc':t_s_rc, 't_s_undo':t_s_undo} 
+  
+# Create DataFrame 
+df = pd.DataFrame(data) 
+df.to_csv('filtered.csv', index=True)
 # =============================================================================
-# per subject    
+# plotting per subject    
 # =============================================================================
 # number est scatter all - final
 fig, axs = plt.subplots(1, 3, sharey=True)
@@ -412,30 +446,30 @@ axs[0].set_ylabel('Trial duration (s)')
 fig.savefig('/Users/sherrybao/Downloads/Research/Road_Construction/rc_all_data/plot/fig/t_rc_hist_all.png',dpi=600)
 plt.close(fig)
 
-# =============================================================================
-fig, axs = plt.subplots(1, 3, sharey=True)
-
-for i in range(0,3):
-    axs[i].boxplot([rm_t[48*i:48*(i+1)], rm_t_undo[48*i:48*(i+1)]],widths = 0.6)  
-    axs[i].plot([1,2],[rm_t[48*i:48*(i+1)], rm_t_undo[48*i:48*(i+1)]], 'o',
-       markerfacecolor = '#727bda',markeredgecolor = 'none',alpha = 0.2)     
-    #plotline1, caplines1, barlinecols1 = ax.errorbar(ind, [mean(mean_t_rc),mean(mean_t_undo)], yerr=[err_rc,err_undo], lolims=True, capsize = 0, ls='None', color='k')
-    #caplines1[0].set_marker('_')
-    #caplines1[0].set_markersize(7)
-    axs[i].set_ylim((0,110))
-    
-    axs[i].set_xticklabels(['w/o undo','w/ undo'])
-    #ax.grid(b=True, which='major', axis = 'y',color='k', linestyle='--')
-    axs[i].set_facecolor('white')
-    axs[i].spines['bottom'].set_color('k')
-    axs[i].spines['left'].set_color('k')
-    axs[i].tick_params(axis='y', colors='k')
-    axs[i].title.set_text('S'+str(i+1))
-axs[0].set_ylabel('Trial duration excluding first-move rt(s)')
-plt.show()
-fig.savefig('/Users/sherrybao/Downloads/Research/Road_Construction/rc_all_data/plot/fig/rm_t_rc_hist_all.png',dpi=600)
-plt.close(fig)
-
+## =============================================================================
+#fig, axs = plt.subplots(1, 3, sharey=True)
+#
+#for i in range(0,3):
+#    axs[i].boxplot([rm_t[48*i:48*(i+1)], rm_t_undo[48*i:48*(i+1)]],widths = 0.6)  
+#    axs[i].plot([1,2],[rm_t[48*i:48*(i+1)], rm_t_undo[48*i:48*(i+1)]], 'o',
+#       markerfacecolor = '#727bda',markeredgecolor = 'none',alpha = 0.2)     
+#    #plotline1, caplines1, barlinecols1 = ax.errorbar(ind, [mean(mean_t_rc),mean(mean_t_undo)], yerr=[err_rc,err_undo], lolims=True, capsize = 0, ls='None', color='k')
+#    #caplines1[0].set_marker('_')
+#    #caplines1[0].set_markersize(7)
+#    axs[i].set_ylim((0,110))
+#    
+#    axs[i].set_xticklabels(['w/o undo','w/ undo'])
+#    #ax.grid(b=True, which='major', axis = 'y',color='k', linestyle='--')
+#    axs[i].set_facecolor('white')
+#    axs[i].spines['bottom'].set_color('k')
+#    axs[i].spines['left'].set_color('k')
+#    axs[i].tick_params(axis='y', colors='k')
+#    axs[i].title.set_text('S'+str(i+1))
+#axs[0].set_ylabel('Trial duration excluding first-move rt(s)')
+#plt.show()
+#fig.savefig('/Users/sherrybao/Downloads/Research/Road_Construction/rc_all_data/plot/fig/rm_t_rc_hist_all.png',dpi=600)
+#plt.close(fig)
+#
 # =============================================================================
 fig, axs = plt.subplots(1, 3, sharey=True)
 
@@ -456,9 +490,9 @@ for i in range(0,3):
         except:  pass
 
 
-    axs[i].plot([1,2],[f_t_rc[48*i:48*(i+1)], f_t_undo[48*i:48*(i+1)]],c = '#cccccc',linewidth=0.3) 
-    axs[i].plot([2.5,3.5],[[median(x) for x in t_everyact_rc[48*i:48*(i+1)]], [median(x) for x in t_everyc_undo[48*i:48*(i+1)]]],c = '#cccccc',linewidth=0.3) 
-    axs[i].plot([4,5],[t_s_rc[48*i:48*(i+1)],t_s_undo[48*i:48*(i+1)]],c = '#cccccc',linewidth=0.3) 
+    axs[i].plot([1,2],[f_t_rc[48*i:48*(i+1)], f_t_undo[48*i:48*(i+1)]],c = '#6a6763',linewidth=0.3) 
+    axs[i].plot([2.5,3.5],[[median(x) for x in t_everyact_rc[48*i:48*(i+1)]], [median(x) for x in t_everyc_undo[48*i:48*(i+1)]]],c = '#6a6763',linewidth=0.3) 
+    axs[i].plot([4,5],[t_s_rc[48*i:48*(i+1)],t_s_undo[48*i:48*(i+1)]],c = '#6a6763',linewidth=0.3) 
 
        
     axs[i].boxplot([f_t_rc[48*i:48*(i+1)],f_t_undo[48*i:48*(i+1)],
@@ -467,7 +501,14 @@ for i in range(0,3):
        t_s_rc[48*i:48*(i+1)],t_s_undo[48*i:48*(i+1)],
        undobox],positions =[1,2,2.5,3.5,4,5,5.5],widths = 0.3,showfliers=False,
        medianprops = dict(color = 'k')) #, color='#0776d8',edgecolor = 'k'
-            
+    
+    test_results = add_stat_annotation(axs[i], data=[f_t_rc[48*i:48*(i+1)],f_t_undo[48*i:48*(i+1)],
+       [median(x) for x in t_everyact_rc[48*i:48*(i+1)]],
+       [median(x) for x in t_everyc_undo[48*i:48*(i+1)]],
+       t_s_rc[48*i:48*(i+1)],t_s_undo[48*i:48*(i+1)]],
+       box_pairs=(f_t_rc[48*i:48*(i+1)],f_t_undo[48*i:48*(i+1)]),
+                                   test='Wilcoxon', text_format='star',
+                                   loc='outside')       
     #    err_fu_1 = np.quantile(f_t_undo[48*i:48*(i+1)],q = 0.25)
 #    err_fu_2 = np.quantile(f_t_undo[48*i:48*(i+1)],q = 0.75)
 #    err_su_1 = np.quantile(t_s_undo[48*i:48*(i+1)],q = 0.25)
@@ -481,18 +522,18 @@ for i in range(0,3):
 #       mean(t_s_undo[48*i:48*(i+1)]),median([y for x in t_everyundo[48*i:48*(i+1)] for y in x])], yerr=[[err_fu_1,err_rcu_1,err_su_1,err_u_1],[err_fu_2,err_rcu_2,err_su_2,err_u_2]], capsize = 3, ls='None', color='k')
     
 #    axs[i].set_ylim((0,60))
-    axs[i].set_xticks([1,2,2.5,3.5,4,5,5.5])
+    axs[i].set_xticks([1.5,3,4.5,5.5])
+#    axs[i].secondary_xaxis(location = 'bottom', xticks = [1,2,2.5,3.5,4,5],
+#       xticklabels = ['without undo', 'with undo', 'without undo', 'with undo','without undo', 'with undo'])
 #    axs[i].set_xticklabels(labels)
     #ax.grid(b=True, which='major', axis = 'y',color='k', linestyle='--')
-    axs[i].set_xticklabels(labels = ['first choice without undo','first choice with undo', 
-       'later choices without undo', 'later choices with undo',
-       'submit without undo','submit with undo','undo'])
+    axs[i].set_xticklabels(labels = ['first choice\nwithout undo with undo','later choices\nwithout undo with undo', 'submit\nwithout undo with undo','undo'])
 
     axs[i].set_facecolor('white')
     axs[i].spines['bottom'].set_color('k')
     axs[i].spines['left'].set_color('k')
     axs[i].tick_params(axis='y', colors='k', direction='in',left = True)   
-    axs[i].tick_params(axis='x', colors='k',labelrotation = 45)
+    axs[i].tick_params(axis='x', colors='k')
     axs[i].title.set_text('S'+str(i+1))
 axs[0].set_ylabel('Response time(s)')
 
@@ -501,7 +542,7 @@ axs[0].set_ylabel('Response time(s)')
 #undo_led = mpatches.Patch(color='#e13f42', label='with undo')
 #plt.legend(handles=[rc_led,undo_led],facecolor = 'white')
 
-fig.set_figwidth(20)
+fig.set_figwidth(26)
 fig.set_figheight(12)
 
 plt.show()
