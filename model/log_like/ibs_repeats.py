43eb938428f2c0e774135cc2ff2a2a4fed45233b
Dongqi Bao <sherrymoriarty@hotmail.com>
2020-07-27 14:29:35 -0400

    organize folder

--- model/log_like/ibs_repeats.py
+++ model/log_like/ibs_repeats.py
@@ -57,3 +57,28 @@ def ibs_repeat(inparams,subject_data,repeats,basic_map):
     print('Final LL: '+str(LL)+', time lapse: '+str(time.time()-start_time))
     return -LL # return negative ll
 
+if __name__ == "__main__":
+    # directories
+    home_dir = '/Users/Toby/Downloads/bao/'
+    input_dir = 'road_construction/rc_all_data/data/data_pilot_preprocessed/'
+    map_dir = 'road_construction/map/active_map/'
+    
+    with open(home_dir + map_dir + 'basic_map_48_all4','r') as file:
+        basic_map = json.load(file) 
+        
+    subs = [1]#,2,4] # subject index 
+    
+    eng = matlab.engine.start_matlab()
+    
+    for sub in subs:
+        sub_data = pd.read_csv(home_dir + input_dir + 'mod_ibs_preprocess_sub_'+str(sub) + '.csv')
+     
+        with open(home_dir + input_dir + 'n_repeat_' + str(sub),'r') as file:
+            repeats = json.load(file) 
+        
+        result = eng.bads("@bads_ll", x0,lb,ub,plb,pub,nargout=2) # BADS will call bads_ll.m to get neg LL
+        	
+        print('result[0]', result[0])
+        print('result[1]', result[1])
+        
+    eng.quit()
