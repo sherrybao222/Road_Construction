93a3f0d15afc1cef3ca5d7ad28f9e44b747a5522
Sherry DB <dongqi.bao@econ.uzh.ch>
2021-01-24 23:45:14 +0100

    change alignment

--- model/bads_run.m
+++ model/bads_run.m
@@ -14,28 +14,28 @@ function bads_run(hpc,job_id,task_id,sub)
 
     % w1, w2, w3,stopping_probability,pruning_threshold,lapse_rate,feature_dropping_rate
 
-    LB = [0, 0, 0, 0, 0.1, 0.01, 0];   % Lower bounds
-    UB = [10, 10, 10, 1, 30, 1, 1];   % Upper bounds
+    LB  = [0,  0,  0,  0, 0.1, 0.01, 0];   % Lower bounds
+    UB  = [10, 10, 10, 1, 30,  1,    1];   % Upper bounds
 
-    PLB = [0, 0, 0, 0, 0.1, 0.01, 0];   % Lower bounds
-    PUB = [10, 10, 10, 1, 20, 0.5, 0.5];   % Upper bounds
+    PLB = [0,  0,  0,  0,  0.1, 0.01, 0];   % Lower bounds
+    PUB = [10, 10, 10, 1,  20,  0.5,  0.5];   % Upper bounds
 
-    nvars = numel(PLB);
+    nvars    = numel(PLB);
     rand_coe = rand(1,nvars);
-    X0 = PLB + rand_coe .* (PUB - PLB)  % Starting point
+    X0       = PLB + rand_coe .* (PUB - PLB)  % Starting point
     
     funwdata = @(x) bads_ll(x,sub);
     
     [x,fval] = bads(funwdata,X0,LB,UB,PLB,PUB);
 
-    Output.x0 = X0;
-    Output.x = x;
+    Output.x0   = X0;
+    Output.x    = x;
     Output.fval = fval;
 
 
     tEnd = toc(tStart);
     % diary off
-    filename = sprintf('result/bads_%.0f_%.0f.mat',job_id,task_id);
+    filename = sprintf('/home/db4058/road_construction/result/bads_%.0f_%.0f.mat',job_id,task_id);
     save(filename)
 
     % pool.delete()
