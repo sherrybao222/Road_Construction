02e7be9e8ccaf58f30a7ed9deb6f7356b19219ee
Sherry DB <sherrymoriarty@hotmail.com>
2022-02-26 22:17:47 +0100

    Update preprocess_data_decision_tree.py
    add decision_tree generator from data

--- analysis/online_2022/preprocessing/preprocess_data_decision_tree.py
+++ analysis/online_2022/preprocessing/preprocess_data_decision_tree.py
@@ -5,4 +5,82 @@ Created on Sat Feb 26 13:20:26 2022
 
 @author: dbao
 """
+import pandas as pd
+from anytree import Node
+from anytree.exporter import JsonExporter
+from glob import glob
+import json
 
+
+# import data
+data_all = []
+
+# directories
+# home_dir = 'G:\My Drive\\researches\\nyu\\road-construction-local-dk\data_online_2022/'
+# map_dir = 'active_map/'
+# data_dir  = 'data/preprocessed'
+home_dir = '/Users/dbao/google_drive_db'+'/road_construction/data/2022_online/'
+map_dir = 'active_map/'
+data_dir  = 'data/preprocessed'
+R_out_dir = home_dir+'R_analysis_data/choice_level/'
+
+
+flist = glob(home_dir + data_dir + '/preprocess4_sub_*.csv')
+# import experiment data
+for fname in flist:
+    with open(fname, 'r') as f:
+        all_data = pd.read_csv(f)
+        data_all.append(all_data)
+
+
+basic_tree = []
+undo_tree = []
+for i in range(len(data_all)): # per subjects
+    subID = i
+    current_trialID = int(data_all[i]['trial_id'][0])
+    current_mapID = int(data_all[i]['map_id'][0])
+    current_condition = data_all[i]['condition'][0]
+    
+    for ti in range(data_all[i].shape[0]): # per choice
+        trialID = int(data_all[i]['trial_id'][ti])
+        mapID = int(data_all[i]['map_id'][ti])
+        condition = data_all[i]['condition'][ti]
+
+        if ti == 0: # start of subject data
+
+            start_node = Node(0, budget = 300, RT= - 1, trialID=current_trialID, mapID = current_mapID)
+            current_node = start_node
+        
+        elif (trialID!=current_trialID)|(mapID!=current_mapID)|(condition!=current_condition): # start of trial
+            
+            exporter = JsonExporter(indent=2, sort_keys=True)
+            json_trial = exporter.export(start_node)
+
+            if current_condition == "basic":
+                basic_tree.append(json_trial)
+            elif current_condition == "undo":
+                undo_tree.append(json_trial)
+                
+            current_trialID = trialID
+            current_mapID = mapID
+            current_condition = condition
+            
+            start_node = Node(0, budget = 300, RT= - 1, trialID=current_trialID, mapID = current_mapID)
+            current_node = start_node
+            
+        elif not data_all[i]['undoIndicator'][ti]:
+            budget = float(data_all[i]['currentBudget'][ti])
+            RT = int(data_all[i]['rt_all'][ti])
+            city = int(data_all[i]['currentChoice'][ti])  
+            
+            new_node = Node(city, budget = budget, RT= RT, subID=subID, mapID = current_mapID, parent = current_node)
+            current_node = new_node
+            
+        elif data_all[i]['undoIndicator'][ti]:
+            current_node = current_node.parent
+
+save_path = '/Users/dbao/google_drive_db/road_construction/data/2022_online/tree_data/'    
+with open(save_path + 'basic_tree','w') as file: 
+    json.dump(basic_tree,file)
+with open(save_path + 'undo_tree','w') as file: 
+    json.dump(undo_tree,file)
